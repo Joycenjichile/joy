2afafa08e0575ffb0098c454627fbddc5c73d32f
joycenjichile <joycenkimalantoi@gmail.com>
2022-02-12 15:34:51 +0300

    main

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var name=("Joyce Nkimalantoi") //String
+    println(name)
+    var age= 21 //int
+    println(age)
+    val myNum= 254797828610L //long
+    println(myNum)
+    var weight=50 //int
+    println(weight)
+    var citizen=("kenyan") //Boolean
+    println(citizen)
+
+
+
+}
